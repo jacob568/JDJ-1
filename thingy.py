ef5867e24c945a408d837f6baa236b487845bd2b
jacob568 <jacob568@users.noreply.github.com>
2020-06-14 16:18:32 +0100

    Update thingy.py

--- thingy.py
+++ thingy.py
@@ -1,1 +1,9 @@
-print("hello")
+x = [1, 2, 3, 4, 5]
+str1 = " big weiner boi up in the trees of the amazon rainforest"
+str2 = " big weiner bois up in the trees of the amazon rainforest"
+
+for i in x:
+	if i == 1:
+		print(str(i) + str1)
+	else:
+		print(str(i) + str2)
