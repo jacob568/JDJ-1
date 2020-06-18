00ceabc7f80252d2ecd75c3e9ff2614666d0edf4
jacob568 <jacob568@users.noreply.github.com>
2020-06-18 19:39:42 +0100

    Added Fibonacci Tests

--- JacobsTestedThings/Fibonacci/__init__.py
+++ JacobsTestedThings/Fibonacci/__init__.py
@@ -15,6 +15,21 @@ def first10Fibonacci():
 
 	return output
 
+def getNthFibonacci(value):
+	if value < 1:
+		return "Please input a positive, non-zero value"
+	currentValue = 0
+	previousValue = 0
+	nextValue = 1
+
+	counter = 1
+	while counter < value:
+		previousValue = currentValue
+		currentValue = nextValue
+		nextValue = previousValue + currentValue
+		counter += 1
+	return currentValue
+
 
 
 
--- JacobsTestedThings/fibonacci-test.py
+++ JacobsTestedThings/fibonacci-test.py
@@ -1,10 +1,33 @@
 import unittest
-from Fibonacci import first10Fibonacci
+from Fibonacci import first10Fibonacci, getNthFibonacci
 
 class TestThingyMethods(unittest.TestCase):
 	def test_correct_result(self):
 		result = first10Fibonacci()
-		self.assertEqual(result, "0 1 1 2 3 5 8 13 21 34 ")
+		self.assertEqual(result, "0 1 1 2 3 5 8 13 21 34 ") # 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
+
+	def test_correct_result1(self):
+		result = getNthFibonacci(0);
+		self.assertEqual(result, "Please input a positive, non-zero value")
+
+	def test_correct_result2(self):
+		result = getNthFibonacci(9)
+		self.assertEqual(result, 21)
+	def test_correct_result3(self):
+		result = getNthFibonacci(1)
+		self.assertEqual(result, 0)
+	def test_correct_result4(self):
+		result = getNthFibonacci(22)
+		self.assertEqual(result, 10946)
+	def test_correct_result5(self):
+		result = getNthFibonacci(20)
+		self.assertEqual(result, 4181)
+	def test_correct_result6(self):
+		result = getNthFibonacci(100)
+		self.assertEqual(result, 218922995834555169026)
+	def test_correct_result7(self):
+		result = getNthFibonacci(73)
+		self.assertEqual(result, 498454011879264)
 
 
 if __name__ == '__main__':
