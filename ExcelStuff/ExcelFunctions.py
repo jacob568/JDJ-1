9c9ed12490e1105d0b6d81b41a8a76943415ee64
jacob568 <jacob568@users.noreply.github.com>
2020-06-20 18:15:52 +0100

    Added a string splitting function

--- ExcelStuff/ExcelFunctions.py
+++ ExcelStuff/ExcelFunctions.py
@@ -32,16 +32,37 @@ temp_sheet = template["DATA"]  # Add Sheet name
 """
 
 def copyRange(startCol, startRow, endCol, endRow, sheet):
-    rangeSelected = []
-    for i in range(startRow, endRow + 1, 1):
-        rowSelected = []
-        for j in range(startCol, endCol + 1, 1):
-            rowSelected.append(sheet.cell(row=i, column=j).value)
-        rangeSelected.append(rowSelected)
-        #FFS HOW DO I PRINT LISTS
-    print(sheet.cell(row=i,column=j))
+	rangeSelected = []
+	for i in range(startRow, endRow + 1, 1):
+		rowSelected = []
+		for j in range(startCol, endCol + 1, 1):
+			rowSelected.append(sheet.cell(row=i, column=j).value)
+			print(sheet.cell(row = i,column = j).value)
+			#if sheet.cell(row = i, column = j).value == "None":
 
-copyRange(2, 1, 2, 10, sheet)
+		rangeSelected.append(rowSelected)
+        #FFS HOW DO I PRINT LISTS
+
+#Building and measurement are stored in the same cell.
+#This function separate the two values into their own variables
+def splitBuildingAndMeasurement(inputValue):
+	words = inputValue.split()
+	buildingName = words[0] + " " +  words[1]
+	measurement = words[2] + " " + words[3] + " " + words[4]
+
+	print("The building is called: " + buildingName)
+	print("The measurements are: " + measurement)
+
+
+def is_number(s):
+    if s.isdigit():
+    	return False
+    else:
+    	return True
+
+
+splitBuildingAndMeasurement("Unit 002 884.27 sq m")
+#copyRange(1, 2, 1, 57, sheet)
 
 #startcol = 3
 #startRow = 2
